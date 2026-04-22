7917603b774a4d3dd68e7f05be8f7a9972cd455e
Surya Prabha <suryaprabha2408@gmail.com>
2026-04-22 20:16:11 +0530

    Updated flask web app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 ﻿from flask import Flask
+import random
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    usage = 85   # sample value
+    usage = random.randint(50, 150)
 
     if usage > 100:
         status = "ALERT 🚨"
@@ -14,8 +16,11 @@ def home():
     return f"""
     <h1>Cloud Monitoring System</h1>
     <h2>Current Usage: {usage}</h2>
-    <h2>Status: {status}</h2>
+    <h2 style='color: {"red" if usage > 100 else "green"}'>
+    Status: {status}
+    </h2>
     """
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

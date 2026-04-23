0a85c3d8bf8c069b9f1759e6041a6544b913f756
suryaprabha2408-code <suryaprabha2408@gmail.com>
2026-04-23 23:01:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-﻿from flask import Flask
-import random
+from flask import Flask
+import psutil
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    usage = random.randint(50, 150)
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
 
-    if usage > 100:
+    if cpu > 80 or memory > 80:
         status = "ALERT 🚨"
+        color = "red"
     else:
         status = "SAFE ✅"
+        color = "green"
 
     return f"""
     <h1>Cloud Monitoring System</h1>
-    <h2>Current Usage: {usage}</h2>
-    <h2 style='color: {"red" if usage > 100 else "green"}'>
-    Status: {status}
-    </h2>
-    
+    <h2>CPU Usage: {cpu}%</h2>
+    <h2>Memory Usage: {memory}%</h2>
+    <h2 style='color: {color}'>Status: {status}</h2>
     """
-    
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    print("CI/CD working")
